06c91d525ca8418420f8fffe5a2998f54dae7744
Shivam <aunshs309123@gmail.com>
2025-07-27 23:22:58 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -30,44 +30,42 @@ app.use(
 );
 app.use(morgan("dev"));
 
-// app.use(async (req, res, next) => {
-//   try {
-//     const decision = await aj.protect(req, {
-//       requested: 1,
-//     });
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1,
+    });
 
-//     if (decision.isDenied()) {
-//       if (decision.reason.isRateLimit()) {
-//         res.status(429).json({ error: "Too Many Requests" });
-//       } else if (decision.reason.isBot()) {
-//         res.status(403).json({ error: "Bot access denied" });
-//       } else {
-//         res.status(403).json({ error: "Forbidden" });
-//       }
-//       return;
-//     }
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too Many Requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
 
-//     if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
-//       res.status(403).json({ error: "Spoofed bot detected" });
-//       return;
-//     }
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
 
-//     next();
-//   } catch (error) {
-//     console.log("Arcjet error", error);
-//     next(error);
-//   }
-// });
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
 
 app.use("/api/products", productRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+});
 
 async function initDB() {
   try {
