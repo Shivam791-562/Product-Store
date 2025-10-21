5b477bc7f232217f205dc7f8151d83b7df34a3bd
Shivam <aunshs309123@gmail.com>
2025-10-21 16:33:10 +0530

    Revert "Update useProductStore.js"
    This reverts commit d2ef9a24d9abb35a068ae846778f7ff8144ddd76.

--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 // base url will be dynamic depending on the environment
-const BASE_URL = import.meta.env.MODE === "development" ? "https://product-store-u2tb.onrender.com" : "";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "";
 
 export const useProductStore = create((set, get) => ({
   // products state
